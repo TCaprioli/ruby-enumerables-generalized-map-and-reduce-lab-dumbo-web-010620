1d9b943eb2ee254be733ef3904721efae42f1dfa
Tyler Caprioli <capriolityler@gmail.com>
2019-12-28 15:24:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+
+def map(array)
+  counter = 0 
+  new_array = []
+  while counter < array.length 
+   array << yield(array[counter])
+   counter += 1 
+  end  
+  new_array 
+end   
+
