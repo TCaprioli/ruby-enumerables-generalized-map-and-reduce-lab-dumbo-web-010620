118031b5682ec78ca8f5cf73c4ef75b49129e1a1
Tyler Caprioli <capriolityler@gmail.com>
2019-12-30 15:25:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,9 +4,23 @@ def map(array)
   counter = 0 
   new_array = []
   while counter < array.length 
-   array << yield(array[counter])
+   new_array << yield(array[counter])
    counter += 1 
   end  
   new_array 
 end   
 
+def reduce(array,starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length
+   sum = yield(sum,array[i])
+   i += 1
+ end
+ sum 
+end   
